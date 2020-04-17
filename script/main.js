e4ccf9490958afbbe4fd397e662d5dc30f685f95
Andrea De Martino <ademartino1992@gmail.com>
2020-04-17 18:04:09 +0200

    Create: Js logic add message with keyboard

--- script/main.js
+++ script/main.js
@@ -3,28 +3,26 @@ $(document).ready(function () {
   
   // General Variables
 
-
-
-  // Add element with button
+  // Add message with button
   $('.app').on('click', '.chat-send', function(){
     console.log('ciao');
     
     addElement();
   })
 
-  // Add element with keyboard
-  // $('.app').on('keyup', '.add-item', function(e){
+  // Add message with keyboard
+  $('.app').on('keyup', '.input-chat__search', function(e){
     
-  //   if (e.which == 13){
-  //     console.log('Premuto Invio su Input');
-  //     addElement();
-  //   }
-  // })
+    if (e.which == 13){
+      console.log('Premuto Invio su Input');
+      addElement();
+    }
+  })
 
   
-  // Add Element Function
+  // Add message Function
   function addElement() {
-    // Clono struttura template li
+    // Clono struttura template
     var messageRow = $('.template .message-row').clone();
     messageRow.addClass('send-message');
     // Prendo valore da inputbox
@@ -36,7 +34,7 @@ $(document).ready(function () {
     } else{
       // Aggiungo valore inputbox a clone
       messageRow.find('h3').text(messageText);
-      // Inserisco il tutto in lista
+      // Inserisco il tutto
       $('.chat-main__wrapper').append(messageRow);
       // Svuoto InputBox
       $('.input-chat__search').val('');
