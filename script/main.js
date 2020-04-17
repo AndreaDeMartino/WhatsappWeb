3d4c57699d0c54c4caed939b13544668d2115c0d
Andrea De Martino <ademartino1992@gmail.com>
2020-04-17 18:00:59 +0200

    Create: Js logic to add message on click

--- script/main.js
+++ script/main.js
@@ -1,18 +1,46 @@
+
 $(document).ready(function () {
-
-  console.log('ciao');
   
+  // General Variables
+  var inputBox = $('.input-chat__search');
 
 
+  // Add element with button
+  // $('.app').on('click', '.inputBox', function(){
+  //   console.log('ciao');
+    
+  //   addElement();
+  // })
 
-
-
-
-
-
-
+  // Add element with keyboard
+  $('.app').on('keyup', '.add-item', function(e){
+    
+    if (e.which == 13){
+      console.log('Premuto Invio su Input');
+      addElement();
+    }
+  })
 
   
-  
+  // Add Element Function
+  function addElement() {
+    // Clono struttura template li
+    var messageRow = $('.template .message-row').clone();
+    messageRow.addClass('send-message');
+    // Prendo valore da inputbox
+    messageText = $('.input-chat__search').val().trim();
+    console.log('Valore Input Box Prelevato:', messageText);
+
+    if (messageText == '') {
+      alert('Attenzione, non hai inserito un testo valido')
+    } else{
+      // Aggiungo valore inputbox a clone
+      messageRow.find('h3').text(messageText);
+      // Inserisco il tutto in lista
+      $('.chat-main__wrapper').append(messageRow);
+      // Svuoto InputBox
+      $('.input-chat__search').val('');
+    }
+  }
 
 });
