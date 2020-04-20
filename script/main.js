4ec87456290eebfc2367516cb7fa25a52be3a380
Andrea De Martino <ademartino1992@gmail.com>
2020-04-20 13:54:22 +0200

    Create: Auto Response Bot

--- script/main.js
+++ script/main.js
@@ -1,32 +1,42 @@
 
 $(document).ready(function () {
   
+var inputMessage = $('#input-chat__write');
+var messageText = inputMessage.val();
 
   // Change Icon on input focus
-  $('#input-chat__write').focus(function(){
+  inputMessage.focus(function(){
     $('.chat-send i').removeClass('fa-microphone').addClass('fa-paper-plane')
   })
 
   // Restore Icon on blur
-  $('#input-chat__write').blur(function(){
+  inputMessage.blur(function(){
     $('.chat-send i').removeClass('fa-paper-plane').addClass('fa-microphone')
   })
 
   
   // Add message with button
   $('.app').on('click', '.chat-send', function(){
-    console.log('ciao');
-    
-    addElement();
+    messageText = inputMessage.val();
+    if(messageText == ''){
+      alert('Attenzione, non hai inserito un testo valido')
+    } else{
+      addElement('user');
+      setTimeout(addElement, 1000, 'bot');
+    }
   })
 
   // Add message with keyboard
-  $('.app').on('keyup', '#input-chat__write', function(e){
+  $('.app').on('keyup', inputMessage, function(e){
     
     if (e.which == 13){
-      console.log('Premuto Invio su Input');
-      addElement();
-
+      messageText = inputMessage.val();
+      if(messageText == ''){
+        alert('Attenzione, non hai inserito un testo valido')
+      } else{
+        addElement('user');
+        setTimeout(addElement, 1000, 'bot');
+      }
     }
   })
 
@@ -35,26 +45,50 @@ $(document).ready(function () {
     $('.user-box').removeClass('user-active');
     $(this).addClass('user-active');
   })
- 
+  
   
   // Add message Function
-  function addElement() {
+  function addElement(type) {
     // Clono struttura template
     var messageRow = $('.template .message-row').clone();
-    messageRow.addClass('send-message');
-    // Prendo valore da inputbox
-    var messageText = $('#input-chat__write').val();
 
-    if (messageText == '') {
-      alert('Attenzione, non hai inserito un testo valido')
-    } else{
-      // Aggiungo valore inputbox a clone
+    // Calcolo orario con funzione AddZero e aggiorno il clone
+    var date = new Date();
+    var time = addZero( date.getHours() );
+    var minutes = addZero( date.getMinutes() );
+    var finalTime = time + ':' + minutes;
+
+    messageRow.find('h5').text(finalTime);
+
+    // Messaggio Inviato da utente
+    if (type == 'user') {
+      // Prendo valore da inputbox
       messageRow.find('h3').text(messageText);
-      // Inserisco il tutto
-      $('.chat-main__wrapper').append(messageRow);
-      // Svuoto InputBox
-      $('#input-chat__write').val('');
+      messageRow.addClass('send-message');
+      
     }
+
+    // Messaggio Inviato da bot
+    if (type == 'bot'){
+      messageRow.addClass('received-message');
+      messageRow.find('h3').text('Ok');
+      // Svuoto InputBox
+      inputMessage.val('');
+    }
+
+
+    // Inserisco il clone su chat reale
+    $('.chat-main__wrapper').append(messageRow);
+    
   }
+
+    // Funzione per aggiungere 0 ad orario se min/ora < 10
+    function addZero(number){
+      if (number < 10) {
+        number = '0' + number;
+      }
+      return number;
+    }
+
 
 });
